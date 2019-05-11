100b847147c93c88fb889d58d16ae5721fd6862b
Deepak Arora <deepak.arora@milwaukeetool.com>
2019-05-11 19:57:44 +0530

    StartJenkins bash script file

---
+++ StartJenkins.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+echo "Hello World"
+cd /Users/darora
+#java -jar jenkins.war --httpPort=8080
+
+#to use office365 email below commond must be used
+java -Dmail.smtp.starttls.enable=true  -jar jenkins.war --httpPort=8080
+
+
+
+java -Djava.awt.headless=true -Dhudson.model.DirectoryBrowserSupport.CSP= -jar /usr/share/jenkins/jenkins.war --webroot=/var/cache/jenkins/war --httpPort=8080 --ajp13Port=-1
