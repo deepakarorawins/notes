76f09f1351b59ab4eda361bfe85f7ab501506a72
Dee Arora <30565176+deepakarorawins@users.noreply.github.com>
2020-11-27 10:54:19 +0530

    Update StartJenkins.sh

--- StartJenkins.sh
+++ StartJenkins.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 echo "Hello World"
-cd /Users/darora
+cd /Users/darora/soft
 #java -jar jenkins.war --httpPort=8080
 
 #to use office365 email below commond must be used
@@ -9,3 +9,6 @@ java -Dmail.smtp.starttls.enable=true  -jar jenkins.war --httpPort=8080
 
 
 java -Djava.awt.headless=true -Dhudson.model.DirectoryBrowserSupport.CSP= -jar /usr/share/jenkins/jenkins.war --webroot=/var/cache/jenkins/war --httpPort=8080 --ajp13Port=-1
+
+#or 
+#java -Djava.awt.headless=true -Dhudson.model.DirectoryBrowserSupport.CSP=" " -jar /usr/share/jenkins/jenkins.war --webroot=/var/cache/jenkins/war --httpPort=8080 --ajp13Port=-1
